70e2369d191c8b2be6d647d3d54c7d0faf7e862a
PHastings37 <patrickhastings@hotmail.co.uk>
2022-11-08 14:28:03 +0000

    testing style

---
+++ proj1/header.hpp
@@ -0,0 +1,5 @@
+int function(int number){
+    number*= 2;
+    std::cout<<number;
+    return number;
+}
---
+++ proj1/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "header.hpp"
+
+int main() {
+    int num = 2;
+    function(num);
+    
+    return 0;
+}
