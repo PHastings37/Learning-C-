2c4b44191ccd1b117fb7440872df9a6a36f291cf
PHastings37 <patrickhastings@hotmail.co.uk>
2022-11-11 16:57:35 +0000

    completed half Input_validation class

--- proj1/header.hpp
+++ proj1/header.hpp
@@ -1,5 +1,62 @@
-int function(int number){
-    number*= 2;
-    std::cout<<number;
-    return number;
-}
+#ifndef HEADER_H
+#define HEADER_H
+#include <iostream>
+#include <limits>
+
+
+
+class Input_validation {
+    private:
+        bool limits{};
+        double upper{std::numeric_limits<double>::max()};
+        double lower{};
+        std::string error{};
+
+    public:
+        Input_validation() = default;
+
+        Input_validation(bool limit_check, double lower_limit, std::string error_msg): 
+            limits(limit_check), lower(lower_limit), error(error_msg)
+        {}
+
+        Input_validation(bool limit_check, double lower_limit, double upper_limit, std::string error_msg)
+            : limits(limit_check), lower(lower_limit), upper(upper_limit), error(error_msg)
+        {}
+
+
+        template <class T> T type_check(){
+            bool type_check{false};
+            T input;
+            T input_T = input;
+            T input_double = input;
+
+            std::cout<<error;
+            std::cin>>input;
+            
+            while(!type_check){
+                if (std::cin.fail()){
+                    clear_input();
+                    std::cout<<error;
+                    std::cin>>input;
+                } else if (static_cast<T>(input_T) != static_cast<double>(input_double)){
+                    clear_input();
+                    std::cout<<error;
+                    std::cin>>input;
+                } else{
+                    break;
+                }   
+            }
+            return input;
+        }   
+
+        
+
+        void clear_input(){
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+        }
+            
+            
+        };
+
+#endif
--- proj1/main.cpp
+++ proj1/main.cpp
@@ -2,8 +2,11 @@
 #include "header.hpp"
 
 int main() {
-    int num = 2;
-    function(num);
-    
+    int atm_num;
+    int upper;
+    int lower;
+    Input_validation Atm_num_validation(true, 1, 118, "Please enter a valid atomic number between 1 and 118: ");
+    Atm_num_validation.type_check<int>();
+
     return 0;
 }
